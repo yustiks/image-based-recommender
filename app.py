505fee6190fe83d7f68cc6b7536d441ab4e0a191
Iustina.Ivanova <yustiks@gmail.com>
2022-06-21 10:39:50 +0200

    flask requirements

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import pandas as pd
 from flask import Flask, render_template
 from flask import request
 
-from sklearn.neighbors import NearestNeighbors
-
 df = pd.read_csv('static/db/train.csv')
 big_array = np.load('static/db/big_array.npy')
 # load the model from disk
@@ -27,22 +25,22 @@ def recommended_crags():
         image_id = int(request.form['image_id'])
         metric_name = 'cosine'
         X = np.array(big_array)
-        #nbrs = NearestNeighbors(n_neighbors=6,
+        # nbrs = NearestNeighbors(n_neighbors=6,
         #                        algorithm='brute',
         #                        metric=metric_name
         #                        ).fit(X)
-        #distances, indices = nbrs.kneighbors([X[image_id - 1]], return_distance=True)
+        # distances, indices = nbrs.kneighbors([X[image_id - 1]], return_distance=True)
         distances, neighbours_id = loaded_model.kneighbors([X[image_id - 1]],
-                                                        n_neighbors=6,
-                                                        return_distance=True)
+                                                           n_neighbors=6,
+                                                           return_distance=True)
         distances = distances[0]
         neighbours_id = neighbours_id[0]
         crags_name = []
         sectors_name = []
         for each_id in neighbours_id:
-            name_crag = df[df.img_id==each_id+1].name_crag.values[0]
+            name_crag = df[df.img_id == each_id + 1].name_crag.values[0]
             crags_name.append(name_crag)
-            name_sector = df[df.img_id==each_id+1].name_sector.values[0]
+            name_sector = df[df.img_id == each_id + 1].name_sector.values[0]
             sectors_name.append(name_sector)
         result_json = {
             'image_id': image_id,
@@ -53,3 +51,7 @@ def recommended_crags():
         }
 
         return render_template('sector_picked.html', data=result_json)
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=8081)
